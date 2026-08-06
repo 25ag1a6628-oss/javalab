0c7ee7226c039f2a265f2c9045802912921f43da
25ag1a6628-oss <25ag1a6628@gmail.com>
2026-08-06 09:15:09 +0000

    threedimension.java

---
+++ threedimension.java
@@ -0,0 +1,11 @@
+
+class threedimension{
+    public static void main(String[] args)
+    {
+        int width=10;
+        int height=20;
+        int depth=15;
+        System.out.println(width*height*depth);
+
+    }
+}
