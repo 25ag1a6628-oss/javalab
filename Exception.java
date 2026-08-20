f593bb94d7a26860fd1fb0006d2509537d314855
25ag1a6628-oss <25ag1a6628@gmail.com>
2026-08-20 08:24:27 +0000

    Exception.java

---
+++ Exception.java
@@ -0,0 +1,12 @@
+
+class Exception
+{
+    public static void main(String args[])
+    {
+        int a=10;
+        int b=2;
+        int c;
+        c=a/b;
+        System.out.println("The value is:"+c);
+    }
+}
